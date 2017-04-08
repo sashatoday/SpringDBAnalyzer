2ca86182f8b98bd5572edbb1e5eaf9db808154c2
sashatoday <zhuravleva357@gmail.com>
2017-04-09 02:10:45 +0300

    create a simple restful web service

---
+++ src/main/java/com/homework/SpringDbAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbAnalyzerApplication.class, args);
+	}
+}
